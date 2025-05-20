02569f494b1331824c549189ca36075988e56d1b
Mr. Nagy <randall.nagy@gmail.com>
2025-05-20 12:31:19 -0400

    Show both views.
    The mission continues ... !

--- Listo/listo.py
+++ Listo/listo.py
@@ -3,11 +3,17 @@
 Mission: A kwargs demonstration.
 
 File: listo.py (aka: ListBlocks3.py)
-Video: https://www.youtube.com/watch?v=UFZnj_5YnCA
 
 State: READY (R.S.C)
 '''
 import os, os.path, sys
+
+version = 0.1
+
+
+views = {'manifest' : [100,'Include file names'],
+         'classy'   : [200,'Class names only']
+         }
 
 
 def list_files(zDir, file_type='.py'):
@@ -21,6 +27,34 @@ def list_files(zDir, file_type='.py'):
             results.append(zFile)
     return results
 
+def fix_blocks(zBlocks:list)->list:
+    ''' Fix parser results. '''
+    results = list()
+    for block in zBlocks:
+        row = list(block)
+        if not row[1]:
+            row[1] = '()'
+        results.append(row)
+    return results
+
+def view_file(fss:int, zBlocks:list):
+    ''' View the classic manifest. '''
+    prev_class = 1
+    for ss, zBlock in enumerate(zBlocks):
+        if zBlock[0].startswith('class'):
+            prev_class = 0
+        if zBlock[0].startswith('def'):
+            print('..... ',end='')
+        print(f'{fss:02}.{ss + prev_class:02} {zBlock[0]}{zBlock[1]}')
+
+
+def view_classy(zBlocks:list):
+    ''' View the clsses, only. '''
+    for zBlock in zBlocks:
+        if zBlock[0].startswith('def'):
+            continue
+        print(f'class {zBlock[0]}{zBlock[1]}')
+
 
 def reporting(**kwargs):
     '''
@@ -29,6 +63,15 @@ NEXT: Multiple reporting types / views? Argparse?
     '''
     import re
     ptrn = re.compile(r'^\s*(class|def)\s+([A-Za-z_][A-Za-z0-9_]*)\s*\(', re.MULTILINE)
+
+    view = views['manifest'][0]
+    if 'view' in kwargs:
+        mode = kwargs['view']
+        if mode in views:
+            print(f'***** A "{mode.upper()}" VIEW *****')
+            view = views[mode][0]
+        else:
+            print(f"Warning: View '{mode}' is not supported.")
     
     if 'class' in kwargs:
         ptrn = re.compile(
@@ -39,30 +82,41 @@ NEXT: Multiple reporting types / views? Argparse?
     path = '.'
     if 'path' in kwargs:
         path = kwargs['path']
+    print(f'***** OF {path} *****')
 
     file_type = '.py'
     if 'type' in kwargs:
         file_type = kwargs['type']
 
-    for fss, zFile in enumerate(sorted(list_files(path)), 1):           
+    fss = 0
+    big_block = []
+    for zFile in sorted(list_files(path)):
         with open(zFile) as fh:
-            prev_class = 1                  # EDGE: Fix ZeroBased def problem.
             data = str(fh.read())
-            zBlocks = ptrn.findall(data)
-            print(zFile.split(os.path.sep)[-1])
-        if not zBlocks:
-            print("\tThere are no blocks.") # EDGE: Confirm there are no blocks.
-            continue
-        for ss, zBlock in enumerate(zBlocks,0):
-            if zBlock[0].startswith('class'):
-                prev_class = 0
-            if zBlock[0].startswith('def'):
-                print('..... ',end='')
-            print(f'{fss:02}.{ss + prev_class:02} {zBlock[0]} {zBlock[1]}')
-            
-    m = sys.argv[0].split(os.path.sep)
-    print("\nFile:",m[-1])
+            zBlocks = fix_blocks(ptrn.findall(data))
+            aFile = zFile.split(os.path.sep)[-1]
+            fss += 1
+            match(view):
+                case 100:
+                    if not zBlocks:
+                        print(f"{fss:02}.00 {aFile}\n\tThere are no blocks.\n")
+                        continue
+                    print(f'{fss:02}.00 {aFile}')
+                    view_file(fss, zBlocks)
+                    print()
+                case 200:
+                    big_block.extend(zBlocks)
+
+    for zBlock in sorted(big_block, key=lambda a: a[0].lower()):
+        print(f'{zBlock[0]}{zBlock[1]}')
+
 
 
 if __name__ == '__main__':
-    reporting(path=r'C:\Users\ranag\Desktop\TotalPythoneering-main\PyTrek0001000\PyTrek')
+    params = dict()
+    params['path'] = r'C:\Users\ranag\Desktop\TotalPythoneering-main\PyTrek0001000\PyTrek'
+    params['class'] = None
+    for view in views:        
+        params['view'] = view
+        reporting(**params)
+    print(f"\nLISTO {version}. File:", sys.argv[0])
